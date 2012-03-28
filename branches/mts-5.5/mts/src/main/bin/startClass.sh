3b2468087cc854c0fa8bbbf47dd9134f4cb48436
gwenhael.pasquiers@devoteam.com <gwenhael.pasquiers@devoteam.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2012-03-28 20:15:51 +0000

    put dependencies in lib/ext directory so that mts's jar will be first

--- branches/mts-5.5/mts/src/main/bin/startClass.sh
+++ branches/mts-5.5/mts/src/main/bin/startClass.sh
@@ -7,6 +7,7 @@ JAVA_HOME=`cat java_home`
 JAVA_ARGUMENTS=`cat java_arguments`
 JAVA_MEMORY=`cat java_memory`
 
+# replace all ";" by ":" in arguments because linux and window's classpath separator are different
 JAVA_ARGUMENTS=$(echo $JAVA_ARGUMENTS|sed 's/;/:/g')
 
 "$JAVA_HOME/bin/java" -Xmx${JAVA_MEMORY}m $JAVA_ARGUMENTS $CLASS $*
