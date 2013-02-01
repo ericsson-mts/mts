a31c66f41ab3fef1d80f5dd1ca53ad9b55358bdb
fabien.henry@devoteam.com <fabien.henry@devoteam.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2013-02-01 14:49:59 +0000

    Version nicolas evano non valid

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/cmd/TextImplementation.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/cmd/TextImplementation.java
@@ -25,7 +25,6 @@ package com.devoteam.srit.xmlloader.cmd;
 
 import com.devoteam.srit.xmlloader.core.PropertiesEnhanced;
 import com.devoteam.srit.xmlloader.core.log.FileTextListenerProvider;
-import com.devoteam.srit.xmlloader.core.log.GenericLogger;
 import com.devoteam.srit.xmlloader.core.log.GlobalLogger;
 import com.devoteam.srit.xmlloader.core.log.TextEvent;
 import com.devoteam.srit.xmlloader.core.log.TextEvent.Topic;
@@ -38,12 +37,9 @@ import com.devoteam.srit.xmlloader.core.utils.exceptionhandler.TextExceptionHand
 import com.devoteam.srit.xmlloader.core.utils.filesystem.LocalFSInterface;
 import com.devoteam.srit.xmlloader.core.utils.filesystem.SingletonFSInterface;
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  *
@@ -68,9 +64,9 @@ public class TextImplementation {
         SingletonFSInterface.setInstance(new LocalFSInterface());
 
         String testFilename = args[0];
-        runner = "-seq";
+        String runnerName = "-seq";
         if (args.length >= 2) {
-            runner = args[1];
+            runnerName = args[1];
         }
 
         List<String> parameterEditable = new LinkedList<String>();
@@ -85,114 +81,98 @@ public class TextImplementation {
                 continue;
             }
             String[] splitted = Utils.splitNoRegex(arg, ":");
-//
-//            if (arg.startsWith("-seq") && "-sequential".startsWith(arg)) {
-//                runnerName = "-seq";
-//            }
-//            else if (arg.startsWith("-par") && "-parallel".startsWith(arg)) {
-//                runnerName = "-par";
-//            }
-//            else if (arg.startsWith("-load")) {     // deprecated
-//                GlobalLogger.instance().getApplicationLogger().warn(Topic.CORE, "DEPRECATED : please use -par[allel] option instead of -load");                
-//                runnerName = "-par";
-//            }
-//            else if (arg.startsWith("-testplan")) {
-//
-//                URI uri = new File(testFilename).toURI();
-//                try {
-//                    TextTester textTester = new TextTester(uri, "-testplan", parameterEditable);
-//                }
-//                catch (Exception ex) {
-//                    GlobalLogger.instance().getApplicationLogger().error(Topic.CORE, ex, "error generate Test plan : path : ", uri.getPath());
-//                }
-//                System.exit(0);
-//            }
-//            else if (splitted.length == 2 && splitted[0].startsWith("-config") && "-configuration".startsWith(splitted[0])) {
-//                try {
-//                    String nameValue = splitted[1];
-//                    String[] splitted1 = Utils.splitNoRegex(nameValue, "+");
-//                    String name = splitted1[0];
-//                    String value = "";
-//                    if (splitted1.length > 1) {
-//                        value = splitted1[1];
-//                    }
-//                    PropertiesEnhanced properties = new PropertiesEnhanced();
-//                    properties.addPropertiesEnhancedComplete(name, value);
-//                    if (Config.getConfigByName("tester.properties").getString(name) == null) {
-//                        usage("The configuration parameter \"" + name + "\" does not exist in the tester.properties files. Please check that point.");
-//                    }
-//                    Config.overrideProperties("tester.properties", properties);
-//                }
-//                catch (Exception e) {
-//                    e.printStackTrace();
-//                    usage("Unable to set the configuration parameters with the -config[uration] option.");
-//                }
-//            }
-//            else if (splitted.length == 2 && splitted[0].startsWith("-param") && "-parameter".startsWith(splitted[0])) {
-//                try {
-//                    String nameValue = splitted[1];
-//                    parameterEditable.add(nameValue);
-//                }
-//                catch (Exception e) {
-//                    e.printStackTrace();
-//                    usage("Unable to set the editable parameter with the -param[eter] option.");
-//                }
-//            }
-//            else if (splitted[0].startsWith("-gen") && "-genReport".startsWith(splitted[0])) {
-//                PropertiesEnhanced properties = new PropertiesEnhanced();
-//                String nameValue = "true";
-//                if (splitted.length > 1) {
-//                    nameValue = splitted[1];
-//                }
-//                properties.addPropertiesEnhancedComplete("stats.AUTOMATIC_GENERATE", nameValue);
-//                Config.overrideProperties("tester.properties", properties);
-//            }
-//            else if (splitted[0].startsWith("-show") && "-showReport".startsWith(splitted[0])) {
-//                PropertiesEnhanced properties = new PropertiesEnhanced();
-//                String nameValue = "true";
-//                if (splitted.length > 1) {
-//                    nameValue = splitted[1];
-//                }
-//                properties.addPropertiesEnhancedComplete("stats.AUTOMATIC_SHOW", nameValue);
-//                Config.overrideProperties("tester.properties", properties);
-//            }
-//            else if (splitted[0].startsWith("-level") && "-levelLog".startsWith(splitted[0])) {
-//                PropertiesEnhanced properties = new PropertiesEnhanced();
-//                String nameValue = "true";
-//                if (splitted.length > 1) {
-//                    nameValue = splitted[1];
-//                }
-//                properties.addPropertiesEnhancedComplete("logs.MAXIMUM_LEVEL", nameValue);
-//                Config.overrideProperties("tester.properties", properties);
-//            }
-//            else if (splitted[0].startsWith("-stor") && "-storageLog".startsWith(splitted[0])) {
-//                PropertiesEnhanced properties = new PropertiesEnhanced();
-//                String nameValue = "true";
-//                if (splitted.length > 1) {
-//                    nameValue = splitted[1];
-//                }
-//                properties.addPropertiesEnhancedComplete("logs.STORAGE_LOCATION", nameValue);
-//                Config.overrideProperties("tester.properties", properties);
-//            }
-//            else {
-//                usage("Invalid option : " + arg);
-//            }
-            try {
-            	  java.lang.reflect.Method method = Class.forName("TextImplementation").getMethod( prefixOption.get( splitted[0].trim() ) , String[].class, List.class, String.class);
-            	  method.invoke( splitted, parameterEditable, testFilename );
-            	} catch (SecurityException e) {
-            	  	
-            	} catch (NoSuchMethodException e) {
-            		usage("Invalid option : " + arg);
-            	} catch (IllegalArgumentException e) {
-            		
-            	} catch (IllegalAccessException e) {
-            		
-            	} catch (InvocationTargetException e) {
-            		
-            	} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-				}
+
+            if (arg.startsWith("-seq") && "-sequential".startsWith(arg)) {
+                runnerName = "-seq";
+            }
+            else if (arg.startsWith("-par") && "-parallel".startsWith(arg)) {
+                runnerName = "-par";
+            }
+            else if (arg.startsWith("-load")) {     // deprecated
+                GlobalLogger.instance().getApplicationLogger().warn(Topic.CORE, "DEPRECATED : please use -par[allel] option instead of -load");                
+                runnerName = "-par";
+            }
+            else if (arg.startsWith("-testplan")) {
+
+                URI uri = new File(testFilename).toURI();
+                try {
+                    TextTester textTester = new TextTester(uri, "-testplan", parameterEditable);
+                }
+                catch (Exception ex) {
+                    GlobalLogger.instance().getApplicationLogger().error(Topic.CORE, ex, "error generate Test plan : path : ", uri.getPath());
+                }
+                System.exit(0);
+            }
+            else if (splitted.length == 2 && splitted[0].startsWith("-config") && "-configuration".startsWith(splitted[0])) {
+                try {
+                    String nameValue = splitted[1];
+                    String[] splitted1 = Utils.splitNoRegex(nameValue, "+");
+                    String name = splitted1[0];
+                    String value = "";
+                    if (splitted1.length > 1) {
+                        value = splitted1[1];
+                    }
+                    PropertiesEnhanced properties = new PropertiesEnhanced();
+                    properties.addPropertiesEnhancedComplete(name, value);
+                    if (Config.getConfigByName("tester.properties").getString(name) == null) {
+                        usage("The configuration parameter \"" + name + "\" does not exist in the tester.properties files. Please check that point.");
+                    }
+                    Config.overrideProperties("tester.properties", properties);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    usage("Unable to set the configuration parameters with the -config[uration] option.");
+                }
+            }
+            else if (splitted.length == 2 && splitted[0].startsWith("-param") && "-parameter".startsWith(splitted[0])) {
+                try {
+                    String nameValue = splitted[1];
+                    parameterEditable.add(nameValue);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    usage("Unable to set the editable parameter with the -param[eter] option.");
+                }
+            }
+            else if (splitted[0].startsWith("-gen") && "-genReport".startsWith(splitted[0])) {
+                PropertiesEnhanced properties = new PropertiesEnhanced();
+                String nameValue = "true";
+                if (splitted.length > 1) {
+                    nameValue = splitted[1];
+                }
+                properties.addPropertiesEnhancedComplete("stats.AUTOMATIC_GENERATE", nameValue);
+                Config.overrideProperties("tester.properties", properties);
+            }
+            else if (splitted[0].startsWith("-show") && "-showReport".startsWith(splitted[0])) {
+                PropertiesEnhanced properties = new PropertiesEnhanced();
+                String nameValue = "true";
+                if (splitted.length > 1) {
+                    nameValue = splitted[1];
+                }
+                properties.addPropertiesEnhancedComplete("stats.AUTOMATIC_SHOW", nameValue);
+                Config.overrideProperties("tester.properties", properties);
+            }
+            else if (splitted[0].startsWith("-level") && "-levelLog".startsWith(splitted[0])) {
+                PropertiesEnhanced properties = new PropertiesEnhanced();
+                String nameValue = "true";
+                if (splitted.length > 1) {
+                    nameValue = splitted[1];
+                }
+                properties.addPropertiesEnhancedComplete("logs.MAXIMUM_LEVEL", nameValue);
+                Config.overrideProperties("tester.properties", properties);
+            }
+            else if (splitted[0].startsWith("-stor") && "-storageLog".startsWith(splitted[0])) {
+                PropertiesEnhanced properties = new PropertiesEnhanced();
+                String nameValue = "true";
+                if (splitted.length > 1) {
+                    nameValue = splitted[1];
+                }
+                properties.addPropertiesEnhancedComplete("logs.STORAGE_LOCATION", nameValue);
+                Config.overrideProperties("tester.properties", properties);
+            }
+            else {
+                usage("Invalid option : " + arg);
+            }
         }
 
         /*
@@ -207,18 +187,18 @@ public class TextImplementation {
 
         try {
             URI uri = new File(testFilename).toURI();
-            TextTester textTester = new TextTester(uri, runner, parameterEditable);
-            System.out.println("Run " + runner + " test \"" + testFilename);
+            TextTester textTester = new TextTester(uri, runnerName, parameterEditable);
+            System.out.println("Run " + runnerName + " test \"" + testFilename);
             int logLevel = GlobalLogger.instance().getLogLevel();
             int logStorage = GlobalLogger.instance().getLogStorage();
-            if ((runner.equals("-load")) && (logLevel < TextEvent.WARN) && (logStorage == GlobalLogger.LOG_STORAGE_FILE)) {
+            if ((runnerName.equals("-load")) && (logLevel < TextEvent.WARN) && (logStorage == GlobalLogger.LOG_STORAGE_FILE)) {
                 String systemOut = "(logs.STORAGE_LOCATION=\"" + logStorage + "\",";
                 systemOut += "logs.MAXIMUM_LEVEL=\"" + logLevel + "\")";
                 System.out.println(systemOut);
             }
 
-            logger.debug(TextEvent.Topic.CORE, "testFilename = ", testFilename);
-            logger.debug(TextEvent.Topic.CORE, "runnerName = ", runner);
+            GlobalLogger.instance().getApplicationLogger().debug(TextEvent.Topic.CORE, "testFilename = ", testFilename);
+            GlobalLogger.instance().getApplicationLogger().debug(TextEvent.Topic.CORE, "runnerName = ", runnerName);
 
 
             textTester.start();
@@ -246,11 +226,11 @@ public class TextImplementation {
 
         try {
             URI uri = new File(testFilename).toURI();
-            MasterTextTester textTester = new MasterTextTester(uri, runner);
+            MasterTextTester textTester = new MasterTextTester(uri, runnerName);
             System.out.println("Run master \"" + testFilename + "\"");
 
-            logger.debug(TextEvent.Topic.CORE, "testFilename = ", testFilename);
-            logger.debug(TextEvent.Topic.CORE, "runnerName = ", runner);
+            GlobalLogger.instance().getApplicationLogger().debug(TextEvent.Topic.CORE, "testFilename = ", testFilename);
+            GlobalLogger.instance().getApplicationLogger().debug(TextEvent.Topic.CORE, "runnerName = ", runnerName);
 
             textTester.run();
 
@@ -287,131 +267,4 @@ public class TextImplementation {
                 + "    [-show[Report]:false|true]\n");
         System.exit(10);
     }
-    
-    //fonction call by refection on the map
-    
-    protected static void sequential( String[] unused, List<String> unusedtow, String unusedTree ){
-    	runner = "-seq";
-    }
-    
-    protected static void parallel( String[] unused, List<String> unusedtow, String unusedTree ){
-    	runner = "-par";
-    }
-
-    protected static void load( String[] unused, List<String> unusedtow, String unusedTree ){
-    	logger.warn(Topic.CORE, "DEPRECATED : please use -par[allel] option instead of -load");                
-        runner = "-par";
-    }
-    
-    protected static void testPlan( String[] unused, List<String> parameterEditable, String testFilename ){
-    	URI uri = new File(testFilename).toURI();
-        try {
-            TextTester textTester = new TextTester(uri, "-testplan", parameterEditable);
-        }
-        catch (Exception ex) {
-            GlobalLogger.instance().getApplicationLogger().error(Topic.CORE, ex, "error generate Test plan : path : ", uri.getPath());
-        }
-        System.exit( 0 );
-    }
-    
-    protected static void configuration( String[] splitted, List<String> unusedtow, String unusedTree ){
-    	if( splitted.length == 2 ){
-    		try {
-    			String nameValue = splitted[1];
-    			String[] splitted1 = Utils.splitNoRegex(nameValue, "+");
-    			String name = splitted1[0];
-    			String value = "";
-    			if (splitted1.length > 1) {
-    				value = splitted1[1];
-    			}
-    			PropertiesEnhanced properties = new PropertiesEnhanced();
-    			properties.addPropertiesEnhancedComplete(name, value);
-    			if (Config.getConfigByName("tester.properties").getString(name) == null) {
-    				usage("The configuration parameter \"" + name + "\" does not exist in the tester.properties files. Please check that point.");
-    			}
-    			Config.overrideProperties("tester.properties", properties);
-    		}
-    		catch (Exception e) {
-    			e.printStackTrace();
-    			usage("Unable to set the configuration parameters with the -config[uration] option.");
-    		}
-    	} else {
-    		usage("Invalid option : " + splitted[0]);
-    	}
-    }
-    
-    protected static void parameter( String[] splitted, List<String> parameterEditable, String unused ){
-    	if( splitted.length == 2 ){
-    		try {
-    			String nameValue = splitted[1];
-    			parameterEditable.add(nameValue);
-    		}
-    		catch (Exception e) {
-    			e.printStackTrace();
-    			usage("Unable to set the editable parameter with the -param[eter] option.");
-    		}
-    	} else {
-    		usage("Invalid option : " + splitted[0]);
-    	}
-    }
-    
-    protected static void genReport( String[] splitted, List<String> unusedtow, String unusedTree ){
-    	PropertiesEnhanced properties = new PropertiesEnhanced();
-        String nameValue = "true";
-        if (splitted.length > 1) {
-            nameValue = splitted[1];
-        }
-        properties.addPropertiesEnhancedComplete("stats.AUTOMATIC_GENERATE", nameValue);
-        Config.overrideProperties("tester.properties", properties);
-    }
-    
-    protected static void showReport( String[] splitted, List<String> unusedtow, String unusedTree ){
-        PropertiesEnhanced properties = new PropertiesEnhanced();
-        String nameValue = "true";
-        if (splitted.length > 1) {
-            nameValue = splitted[1];
-        }
-        properties.addPropertiesEnhancedComplete("stats.AUTOMATIC_SHOW", nameValue);
-        Config.overrideProperties("tester.properties", properties);
-    }
-    
-    protected static void levelLog( String[] splitted, List<String> unusedtow, String unusedTree ){
-        PropertiesEnhanced properties = new PropertiesEnhanced();
-        String nameValue = "true";
-        if (splitted.length > 1) {
-            nameValue = splitted[1];
-        }
-        properties.addPropertiesEnhancedComplete("logs.MAXIMUM_LEVEL", nameValue);
-        Config.overrideProperties("tester.properties", properties);
-    }
-    
-    protected static void storageLog( String[] splitted, List<String> unusedtow, String unusedTree ){
-        PropertiesEnhanced properties = new PropertiesEnhanced();
-        String nameValue = "true";
-        if (splitted.length > 1) {
-            nameValue = splitted[1];
-        }
-        properties.addPropertiesEnhancedComplete("logs.STORAGE_LOCATION", nameValue);
-        Config.overrideProperties("tester.properties", properties);
-    }
-    
-    @SuppressWarnings("serial")
-	private static Map<String,String> prefixOption = new TreeMap<String,String>(){
-    	{
-    		put( "-sequential" , "sequential" );
-    		put( "-parallel" , "parallel" );
-    		put( "-load" , "load" );
-    		put( "-testplan" , "testPlan" );
-    		put( "-configuration" , "configuration" );
-    		put( "-parameter" , "parameter" );
-    		put( "-genReport" , "genReport" );
-    		put( "-showReport" , "showReport" );
-    		put( "-levelLog" , "levelLog" );
-    		put( "-storageLog" , "storageLog" );
-    	}
-    };
-    
-    private static GenericLogger logger = GlobalLogger.instance().getApplicationLogger();
-    
-    private static String runner = "-seq";
 }
