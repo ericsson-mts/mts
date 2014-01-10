5ed443349290b8b66d00d14a237f789ba0251cc4
gwenhael.pasquiers@ericsson.com <gwenhael.pasquiers@ericsson.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2014-01-10 09:47:10 +0000

    Fix parsing so that a Byte-range is automatically set if necessary

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/msrp/data/MSRPTextMessage.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/msrp/data/MSRPTextMessage.java
@@ -20,7 +20,6 @@
  * If not, see <http://www.gnu.org/licenses/>.
  * 
  */
-
 package com.devoteam.srit.xmlloader.msrp.data;
 
 import java.util.HashMap;
@@ -29,106 +28,129 @@ import java.util.HashSet;
 import com.devoteam.srit.xmlloader.core.Parameter;
 import com.devoteam.srit.xmlloader.core.utils.Utils;
 import com.devoteam.srit.xmlloader.core.coding.text.Header;
+import com.devoteam.srit.xmlloader.core.log.GlobalLogger;
+import com.devoteam.srit.xmlloader.core.log.TextEvent;
 
-public class MSRPTextMessage{
-			
-	//--- attribut --- //
-	private String msg = null;
-	private MSRPFirstLine firstline = null;
-	private String headers = null;
-	private MSRPContentParser contentParser = null;   
-	
-	/*
-	 * Protocol string to parse 
-	 * 
-	 */
-	private String protocol;	
-		
-	/**
-	 * List of the multi-value headers : the different values are on the same line 
-	 * separated with the ',' character (SIP only) 
-	 */
-	private HashSet<String> multiHeader = new HashSet<String>();
-	/**
-	 * Table of the compressed headers : some headers are compressed with just a leter
-	 * This table has the corresponding long headers values (SIP only) 
-	 */	
-	private HashMap<String, String> compressedHeader = new HashMap<String, String>();
+public class MSRPTextMessage {
 
-	private MSRPMsgParser parser;
-	
-	// --- construct --- //
-	public MSRPTextMessage(String protocol) throws Exception
-	{
-		this.protocol = protocol;
-	}
+    //--- attribut --- //
+    private String msg = null;
+    private MSRPFirstLine firstline = null;
+    private String headers = null;
+    private MSRPContentParser contentParser = null;
 
-	public void parse(String msg) throws Exception 
-	{
-		// case a header is contining at the next line
-		
-        msg = Utils.replaceNoRegex(msg, "\r\n", "\n");        
-		msg = Utils.replaceNoRegex(msg, "\n", "\r\n");
-		
-		msg = msg.replace('\t', ' ');
-		msg = Utils.replaceNoRegex(msg, "\r\n ", " ");		
+    /*
+     * Protocol string to parse 
+     * 
+     */
+    private String protocol;
 
-		// get of the content of the message
-		int iPosContent = msg.indexOf("\r\n\r\n");
-		if (iPosContent < 0)
-		{
-			iPosContent = msg.length();
-		}
-		String content = msg.substring(iPosContent).trim();
-        
-		// get of the headers of the message 
-		this.headers = msg.substring(0, iPosContent).trim();
-		// Calculate the Content-Length header is not present in the message or has an invalid value
-		
-		// parse the headers
-		this.parser = new MSRPMsgParser(multiHeader, compressedHeader); 
-		parser.parse(headers, "\r", ':', "<>", "\"\"");		
-		parser.processHeaders();		
-        
-		// parse the firstline of the message
-		String fl = parser.getHeader(null).getHeader(0);
-		this.firstline = new MSRPFirstLine(fl, protocol);
+    /**
+     * List of the multi-value headers : the different values are on the same line separated with the ',' character (SIP only)
+     */
+    private HashSet<String> multiHeader = new HashSet<String>();
+    /**
+     * Table of the compressed headers : some headers are compressed with just a leter This table has the corresponding long headers values (SIP only)
+     */
+    private HashMap<String, String> compressedHeader = new HashMap<String, String>();
 
-		// parsing of the content of the message
-		Header contentType = parser.getHeader("Content-Type");
-		Header boundary = contentType.parseParameter("boundary", ";", '=', "<>", "\"\"");
+    private MSRPMsgParser parser;
+
+    // --- construct --- //
+    public MSRPTextMessage(String protocol) throws Exception {
+        this.protocol = protocol;
+    }
+
+    public void parse(String msg) throws Exception {
+
+        // we can trim because msrp starts and ends with non-whitespace characters
+        msg = msg.trim();
+
+        // find index of separation between headers and content
+        int iDoubleNL = msg.indexOf("\n\n");
+        if (iDoubleNL < 0) {
+            iDoubleNL = msg.indexOf("\r\n\r\n");
+        }
+        if (iDoubleNL < 0) {
+            iDoubleNL = msg.length();
+        }
+
+        String content = msg.substring(iDoubleNL).trim();
+
+        // get of the headers of the message 
+        this.headers = msg.substring(0, iDoubleNL).trim();
+
+        // make sure we have only CRLF within headers
+        this.headers = Utils.replaceNoRegex(this.headers, "\r\n", "\n");
+        this.headers = Utils.replaceNoRegex(this.headers, "\n", "\r\n");
+
+        // parse the headers
+        this.parser = new MSRPMsgParser(multiHeader, compressedHeader);
+        parser.parse(this.headers, "\r", ':', "<>", "\"\"");
+        parser.processHeaders();
+
+        // parse the firstline of the message
+        String fl = parser.getHeader(null).getHeader(0);
+        this.firstline = new MSRPFirstLine(fl, protocol);
+
+        // parsing of the content of the message
+        Header contentType = parser.getHeader("Content-Type");
+        Header boundary = contentType.parseParameter("boundary", ";", '=', "<>", "\"\"");
         contentParser = new MSRPContentParser(content, boundary.getHeader(0));
-		
-        // calculate the complete message
-		StringBuilder buff = new StringBuilder(this.headers);
-		if(iPosContent != msg.length())//just for message with no content
-            buff.append("\r\n\r\n").append(content);
-        buff.append("\r\n");
-		this.msg = buff.toString();
-	}
 
-	public void addContentParameter(Parameter var, String[] params, String path) throws Exception 
-	{
-		contentParser.addContentParameter(var, params, path);
-	}
+        
+        if (this.headers.contains("Byte-Range")) {
+            Header byteRangeHeader = parser.getHeader("Byte-Range");
+            String value = byteRangeHeader.getHeader(0).trim();
+
+            boolean shouldReplace = false;
+
+            try {
+                String start = value.split("-")[0];
+                String end = value.split("/")[0].split("-")[1];
+                String total = value.split("/")[1];
+                if (!Utils.isInteger(total) || !Utils.isInteger(start) || !Utils.isInteger(end)) {
+                    shouldReplace = true;
+                }
+            }
+            catch (Exception e) {
+                shouldReplace = true;
+            }
+
+            if (shouldReplace) {
+                int contentSize = content.indexOf("-------");
+                this.headers = this.headers.replaceFirst("Byte-Range.*\\r\\n", "Byte-Range: 1-" + contentSize + "/" + contentSize + "\r\n");
+            }
+        }
+
+        // compute the complete message
+        StringBuilder buff = new StringBuilder(this.headers);
+        if (!content.isEmpty()) {
+            buff.append("\r\n\r\n");
+            buff.append(content);
+        }
+        buff.append("\r\n");
+        this.msg = buff.toString();
+    }
+
+    public void addContentParameter(Parameter var, String[] params, String path) throws Exception {
+        contentParser.addContentParameter(var, params, path);
+    }
 
     /*
      * 	Calculate the Content-Length header is not present in the message or has an invalid value
      * 
      */
-    public static String getHeaderValue(String headers, String hName) throws Exception
-    {
-    	// get the Content-Length header value
+    public static String getHeaderValue(String headers, String hName) throws Exception {
+        // get the Content-Length header value
         int iPosLength = headers.indexOf("\r\n" + hName);
-        if (iPosLength >= 0)
-        {
-        	int iPosLengthValue = headers.indexOf(":", iPosLength + 2);        	
-        	int iPosLengthEnd = headers.indexOf("\r\n", iPosLength + 2);
-        	if (iPosLengthEnd < 0)
-        	{
-        		iPosLengthEnd = headers.length() - 1;
-        	} 
-        	return headers.substring(iPosLengthValue + 1, iPosLengthEnd + 1).trim();
+        if (iPosLength >= 0) {
+            int iPosLengthValue = headers.indexOf(":", iPosLength + 2);
+            int iPosLengthEnd = headers.indexOf("\r\n", iPosLength + 2);
+            if (iPosLengthEnd < 0) {
+                iPosLengthEnd = headers.length() - 1;
+            }
+            return headers.substring(iPosLengthValue + 1, iPosLengthEnd + 1).trim();
         }
         return null;
     }
@@ -137,113 +159,102 @@ public class MSRPTextMessage{
      * 	Remove the first occurence of a header from an index
      * 
      */
-    private static String removeHeader(String headers, String name) throws Exception
-    {
-    	// get the Content-Length header value
+    private static String removeHeader(String headers, String name) throws Exception {
+        // get the Content-Length header value
         int iPosLength = headers.indexOf("\r\n" + name);
-        if (iPosLength >= 0)
-        {        	
-        	int iPosLengthEnd = headers.indexOf("\r\n", iPosLength + 2);
-        	if (iPosLengthEnd < 0)
-        	{
-        		iPosLengthEnd = headers.length();
-        	} 
-        	// remove the Content-Length header
-    		return headers.substring(0, iPosLength) + headers.substring(iPosLengthEnd, headers.length()); 
+        if (iPosLength >= 0) {
+            int iPosLengthEnd = headers.indexOf("\r\n", iPosLength + 2);
+            if (iPosLengthEnd < 0) {
+                iPosLengthEnd = headers.length();
+            }
+            // remove the Content-Length header
+            return headers.substring(0, iPosLength) + headers.substring(iPosLengthEnd, headers.length());
         }
-        else
-        {
-        	return headers;
+        else {
+            return headers;
         }
     }
-    
+
     /*
      * 	Remove the all occurences of a header from an index
      * 
      */
-    public static String removeAllHeader(String headers, String name) throws Exception
-    {   
-    	int iPosLength = 0; 
-    	int iPosLengthEnd = 0;
-    	while (iPosLength >= 0) 
-    	{
-	    	// get the Content-Length header value
-	        iPosLength = headers.indexOf("\r\n" + name, iPosLength);
-	        if (iPosLength >= 0)
-	        {        	
-	        	iPosLengthEnd = headers.indexOf("\r\n", iPosLength + 2);
-	        	if (iPosLengthEnd < 0)
-	        	{
-	        		iPosLengthEnd = headers.length() - 1;
-	        	} 
-	        	// remove the Content-Length header
-	    		headers = headers.substring(0, iPosLength) + headers.substring(iPosLengthEnd, headers.length()); 
-	        }
-    	}
-       	return headers;
+    public static String removeAllHeader(String headers, String name) throws Exception {
+        int iPosLength = 0;
+        int iPosLengthEnd = 0;
+        while (iPosLength >= 0) {
+            // get the Content-Length header value
+            iPosLength = headers.indexOf("\r\n" + name, iPosLength);
+            if (iPosLength >= 0) {
+                iPosLengthEnd = headers.indexOf("\r\n", iPosLength + 2);
+                if (iPosLengthEnd < 0) {
+                    iPosLengthEnd = headers.length() - 1;
+                }
+                // remove the Content-Length header
+                headers = headers.substring(0, iPosLength) + headers.substring(iPosLengthEnd, headers.length());
+            }
+        }
+        return headers;
     }
 
-	/*
+    /*
      * 	Add all headers into Parameter
      * 
      */
-    public void addHeaderIntoParameter(Parameter var) throws Exception
-    {
-    	parser.addHeaderIntoParameter(var);
+    public void addHeaderIntoParameter(Parameter var) throws Exception {
+        parser.addHeaderIntoParameter(var);
     }
 
-	/*
+    /*
      * 	Add other headers into Parameter
      * 
      */
-    public void addOtherIntoParameter(Parameter var) throws Exception
-    {
-    	parser.addOtherIntoParameter(var);
+    public void addOtherIntoParameter(Parameter var) throws Exception {
+        parser.addOtherIntoParameter(var);
     }
 
-    
-	public String getMessage(){
-		return this.msg;
-	}
-	
-	public MSRPFirstLine getFirstline() {
-		return firstline;
-	}
+    public String getMessage() {
+        return this.msg;
+    }
 
-	public Header getHeader(String name){
-		Header header = parser.getHeader(name);
-		if (header != null)
-		{
-			return header;
-		}
-		else
-		{
-			return new Header(name);
-		}
-	}
-			
-	public String getHeaders() throws Exception {
-		return this.headers;
-	}
+    public MSRPFirstLine getFirstline() {
+        return firstline;
+    }
 
-	public void setCompressedHeader(HashMap<String, String> compressedHeader) {
-		this.compressedHeader = compressedHeader;
-	}
+    public Header getHeader(String name) {
+        Header header = parser.getHeader(name);
+        if (header != null) {
+            return header;
+        }
+        else {
+            return new Header(name);
+        }
+    }
 
-	public void setMultiHeader(HashSet<String> multiHeader) {
-		this.multiHeader = multiHeader;
-	}
-	
-	public char getContinuationFlag(){
-		String endLine = msg.substring(msg.indexOf("-------"), msg.length());
-		if(endLine.contains("$")){
-			return '$';
-		}else if(endLine.contains("+")){
-			return '+';
-		}else {
-			return '#';
-		}
-		
-	}
-		
+    public String getHeaders() throws Exception {
+        return this.headers;
+    }
+
+    public void setCompressedHeader(HashMap<String, String> compressedHeader) {
+        this.compressedHeader = compressedHeader;
+    }
+
+    public void setMultiHeader(HashSet<String> multiHeader) {
+        this.multiHeader = multiHeader;
+    }
+
+    public char getContinuationFlag() {
+        String endLine = msg.substring(msg.indexOf("-------"), msg.length());
+        if (endLine.contains("$")) {
+            return '$';
+        }
+        else if (endLine.contains("+")) {
+            return '+';
+        }
+        else {
+            return '#';
+        }
+
+    }
+
 }
