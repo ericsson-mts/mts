5f985655909badfebe2f6f830817d31001e924ba
fabien.henry <fabien.henry@ericsson.com>
2014-08-12 09:19:46 +0000

    Classe abstraite

---
+++ mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/ApMessage.java
@@ -0,0 +1,60 @@
+/* 
+ * Copyright 2012 Devoteam http://www.devoteam.com
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * 
+ * This file is part of Multi-Protocol Test Suite (MTS).
+ * 
+ * Multi-Protocol Test Suite (MTS) is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License.
+ * 
+ * Multi-Protocol Test Suite (MTS) is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Multi-Protocol Test Suite (MTS).
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.devoteam.srit.xmlloader.sigtran.ap;
+
+import gp.utils.arrays.Array;
+import gp.utils.arrays.DefaultArray;
+
+import java.util.List;
+
+import org.dom4j.Element;
+import org.mobicents.protocols.asn.AsnOutputStream;
+import org.mobicents.protocols.ss7.tcap.asn.ApplicationContextName;
+import org.mobicents.protocols.ss7.tcap.asn.ApplicationContextNameImpl;
+import org.mobicents.protocols.ss7.tcap.asn.DialogPortion;
+import org.mobicents.protocols.ss7.tcap.asn.DialogRequestAPDU;
+import org.mobicents.protocols.ss7.tcap.asn.TCBeginMessageImpl;
+import org.mobicents.protocols.ss7.tcap.asn.TcapFactory;
+import org.mobicents.protocols.ss7.tcap.asn.comp.Component;
+import org.mobicents.protocols.ss7.tcap.asn.comp.Invoke;
+import org.mobicents.protocols.ss7.tcap.asn.comp.OperationCode;
+import org.mobicents.protocols.ss7.tcap.asn.comp.Parameter;
+import org.mobicents.protocols.ss7.tcap.asn.comp.TCBeginMessage;
+
+
+/**
+ *
+ * @author gansquer
+ */
+public abstract class ApMessage {
+
+    public abstract Array encode() throws Exception; 
+
+    public abstract void decode(Array array) throws Exception;
+
+    public abstract void parseFromXML(Element root) throws Exception;
+    
+    public abstract String toXML(Element root) throws Exception;
+    	
+}
