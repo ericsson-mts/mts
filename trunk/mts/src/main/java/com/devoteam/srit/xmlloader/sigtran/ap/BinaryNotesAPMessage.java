3142201b8f114020e91da3cb53e9d4c565c9b523
fabien.henry@ericsson.com <fabien.henry@ericsson.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2014-08-18 16:07:19 +0000

    New version encodage OK (mix entre BinaryNotes et Mobicents (DialoguePDU)

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/BinaryNotesAPMessage.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/BinaryNotesAPMessage.java
@@ -25,6 +25,7 @@ package com.devoteam.srit.xmlloader.sigtran.ap;
 
 import gp.utils.arrays.Array;
 import gp.utils.arrays.DefaultArray;
+import gp.utils.arrays.SupArray;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -35,11 +36,35 @@ import java.util.List;
 import org.bn.CoderFactory;
 import org.bn.IDecoder;
 import org.bn.IEncoder;
+import org.bn.types.BitString;
+import org.bn.types.ObjectIdentifier;
 import org.dom4j.Element;
+import org.mobicents.protocols.asn.AsnOutputStream;
+import org.mobicents.protocols.ss7.tcap.asn.ApplicationContextName;
+import org.mobicents.protocols.ss7.tcap.asn.ApplicationContextNameImpl;
+// import org.mobicents.protocols.ss7.tcap.asn.DialogPortion;
+import org.mobicents.protocols.ss7.tcap.asn.DialogRequestAPDU;
+import org.mobicents.protocols.ss7.tcap.asn.TcapFactory;
 
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Component;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.IMSI;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.ISDN_AddressString;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Invoke;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.InvokeIdType;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.InvokeParameter;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.LMSI;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Mo_forwardSM_Arg;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Operation;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.OperationLocalvalue;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.ServiceCentreAddress;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Sm_RP_DA;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Sm_RP_OA;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Sm_RP_UI;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.AARQ_apdu;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.AARQ_apdu.AARQ_apduSequenceType;
 import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.Begin;
-import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.Component;
 import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.ComponentPortion;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.DialoguePDU;
 import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.DialoguePortion;
 import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.OrigTransactionID;
 import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.TCMessage;
@@ -51,116 +76,148 @@ import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.TCMessage;
  */
 public class BinaryNotesAPMessage extends APMessage {
 
+	// TCAP layer
 	TCMessage tcMessage;
-	DialoguePortion dp;
-	ArrayList<Component> comps;
+	Begin tcapBegin;
+	// DialoguePDU dialoguePdu;
+	org.mobicents.protocols.ss7.tcap.asn.DialogPortion dp;
+	ArrayList<com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.Component> comps;
+	
+	//MAP layer
+	Component mapComponent; 
 
-    public BinaryNotesAPMessage() {    	
-    	Begin begin = new Begin();
+    public BinaryNotesAPMessage() 
+    {
+    	
+    	// Define TCMessages (TCAP.asn file)
+    	this.tcapBegin = new Begin();
     	OrigTransactionID otid = new OrigTransactionID();
     	byte[] transID = new byte[]{0,0,0,1};
     	otid.setValue(transID);
-    	begin.setOtid(otid);
-    	
-    	this.dp = new DialoguePortion();
-    	dp.setValue(new byte[]{(byte) 0x6b,(byte) 0x1e,(byte) 0x28,(byte) 0x1c,(byte) 0x06,(byte) 0x07,(byte) 0x00,(byte) 0x11,(byte) 0x86,(byte) 0x05,(byte) 0x01,(byte) 0x01,(byte) 0x01,(byte) 0xa0,(byte) 0x11,(byte) 0x60,(byte) 0x0f,(byte) 0x80, (byte) 0x02, (byte) 0x07, (byte) 0x80, (byte) 0xa1, (byte) 0x09, (byte) 0x06, (byte) 0x07, (byte) 0x04, (byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x15, (byte) 0x02});
-    	begin.setDialoguePortion(dp);
-   
+    	this.tcapBegin.setOtid(otid);
     	ComponentPortion cp = new ComponentPortion();
-    	Component comp = new Component();
-    	comp.setValue(new byte[]{(byte) 0xa1,(byte) 0x30,(byte) 0x02,(byte) 0x01,(byte) 0x01,(byte) 0x02,(byte) 0x01,(byte) 0x2e,(byte) 0x30,(byte) 0x28, (byte) 0x84,(byte) 0x07,(byte) 0x91,(byte) 0x33,(byte) 0x66,(byte) 0x60,(byte) 0x05,(byte) 0x67,(byte) 0xf9,(byte) 0x82, (byte) 0x07,(byte) 0x91,(byte) 0x33,(byte) 0x66,(byte) 0x31,(byte) 0x70,(byte) 0x71,(byte) 0xf3,(byte) 0x04,(byte) 0x14,(byte) 0x11,(byte) 0x08,(byte) 0x0b,(byte) 0x91,(byte) 0x33,(byte) 0x66,(byte) 0x60,(byte) 0x05,(byte) 0x67,(byte) 0xf7,(byte) 0x00,(byte) 0x00,(byte) 0xa9,(byte) 0x06,(byte) 0xf3,(byte) 0xf9,(byte) 0x7c,(byte) 0x3e,(byte) 0x9f,(byte) 0x03});
-    	this.comps = new ArrayList<Component>();
+    	com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.Component comp = new com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.Component();
+    	this.comps = new ArrayList<com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.Component>();
     	this.comps.add(comp);
     	cp.setValue(comps);
-    	begin.setComponents(cp);
-
+    	this.tcapBegin.setComponents(cp);
     	this.tcMessage = new TCMessage();
-    	this.tcMessage.selectBegin(begin); 
-
+    	this.tcMessage.selectBegin(tcapBegin); 
+    	// this.dialoguePdu.initWithDefaults();
     	
-    	/*= (TCBeginMessageImpl) TcapFactory.createTCBeginMessage();
+    	// Define TCMessages (TCAP.asn file)
+    	/* BUG en encodage avec BN : Remplacement par Mobicents
+    	this.dialoguePdu = new DialoguePDU();
+    	this.dialoguePdu.initWithDefaults();
+    	AARQ_apdu aarq = new AARQ_apdu();
+    	AARQ_apduSequenceType aarq_apduSequenceType = new AARQ_apduSequenceType();
+    	BitString pv = new BitString();
+    	pv.setValue(new byte[]{(byte) 0x80});
+    	aarq_apduSequenceType.setProtocol_version(pv);
+    	ObjectIdentifier oi = new ObjectIdentifier();
+    	oi.setValue("0.4.0.0.1.0.21.2");
+    	aarq_apduSequenceType.setApplication_context_name(oi);
+    	//ArrayList<byte[]> userInformations = new ArrayList<byte[]>(); 
+    	aarq_apduSequenceType.setUser_information(null);
+    	aarq.setValue(aarq_apduSequenceType);
+    	this.dialoguePdu.selectDialogueRequest(aarq);
+    	*/
+    	this.dp = TcapFactory.createDialogPortion();
+        this.dp.setUnidirectional(false);
+        DialogRequestAPDU apdu =  TcapFactory.createDialogAPDURequest();
+        // Protocol-version = true
+        apdu.setDoNotSendProtocolVersion(false);
+        this.dp.setDialogAPDU(apdu);
+        
+        // application-context-name=0.4.0.0.1.0.21.2
+        ApplicationContextName acn = new ApplicationContextNameImpl();
+        acn.setOid(new long[] {0,4,0,0,1,0,21,2});
+        apdu.setApplicationContextName(acn);
+        /*
+        if (event.getUserInformation() != null) {
+            apdu.setUserInformation(event.getUserInformation());
+            this.lastUI = event.getUserInformation();
+        }
+        */
+        this.dp.setOidValue(new long[] {0,0,17,773,1,1,1});
 
-        // build DP
-
-        //if (event.getApplicationContextName() != null) {
-            //this.dpSentInBegin = true;
-            DialogPortion dp = TcapFactory.createDialogPortion();
-            dp.setUnidirectional(false);
-            DialogRequestAPDU apdu =  TcapFactory.createDialogAPDURequest();
-            // Protocol-version = true
-            apdu.setDoNotSendProtocolVersion(false);
-            dp.setDialogAPDU(apdu);
-            
-            // application-context-name=0.4.0.0.1.0.21.2
-            ApplicationContextName acn = new ApplicationContextNameImpl();
-            acn.setOid(new long[] {0,4,0,0,1,0,21,2});
-            apdu.setApplicationContextName(acn);
-            if (event.getUserInformation() != null) {
-                apdu.setUserInformation(event.getUserInformation());
-                this.lastUI = event.getUserInformation();
-            }
-            tcbm.setDialogPortion(dp);
-
-        //    if (this.provider.getStack().getStatisticsEnabled()) {
-        //        String acn = ((ApplicationContextNameImpl) event.getApplicationContextName()).getStringValue();
-        //        this.provider.getStack().getCounterProviderImpl().updateOutgoingDialogsPerApplicatioContextName(acn);
-        //    }
-        //} else {
-        //    if (this.provider.getStack().getStatisticsEnabled()) {
-        //        this.provider.getStack().getCounterProviderImpl().updateOutgoingDialogsPerApplicatioContextName("");
-        //    }
-        //}
-
-        // now comps
-    	// transaction ID= 00000001	
-    	byte[] transID = new byte[]{0,0,0,1};
-        tcbm.setOriginatingTransactionId(transID);
-        // if (this.scheduledComponentList.size() > 0) {
-            Component[] componentsToSend = new Component[1];
-            componentsToSend[0] = TcapFactory.createComponentInvoke();
-            ((Invoke) componentsToSend[0]).setInvokeId((long) 1);
-            // ((Invoke) componentsToSend[0]).setLinkedId((long) 1);
-            OperationCode opCode = TcapFactory.createOperationCode();
-            opCode.setLocalOperationCode((long) 0x2e);
-            ((Invoke) componentsToSend[0]).setOperationCode(opCode);
-            //this.prepareComponents(componentsToSend);
-            Parameter[] params = new Parameter[3];
-            params[0] = TcapFactory.createParameter();
-            params[0].setTag(0x04);
-            params[0].setTagClass(0x02);
-            params[0].setPrimitive(true);
-            params[0].setSingleParameterInAsn();
-            params[0].setData(new byte[]{(byte) 0x91, (byte) 0x33, (byte) 0x66, (byte) 0x60, (byte) 0x05, (byte) 0x67, (byte) 0xf9});
-            params[1] = TcapFactory.createParameter();
-            params[1].setTag(0x02);
-            params[1].setTagClass(0x02);
-            params[1].setPrimitive(true);
-            params[1].setSingleParameterInAsn();
-            params[1].setData(new byte[]{(byte) 0x91, (byte) 0x33, (byte) 0x66, (byte) 0x31, (byte) 0x70, (byte) 0x71, (byte) 0xf3});
-            params[2] = TcapFactory.createParameter();
-            params[2].setTag(0x04);
-            params[2].setData(new byte[]{(byte) 0x11, (byte) 0x08, (byte) 0x0b, (byte) 0x91, (byte) 0x33, (byte) 0x66, (byte) 0x60, (byte) 0x05, (byte) 0x67, (byte) 0xf7, (byte) 0x00,  (byte) 0x00,  (byte) 0xa9,  (byte) 0x06,  (byte) 0xf3,  (byte) 0xf9,  (byte) 0x7c,  (byte) 0x3e,  (byte) 0x9f,  (byte) 0x03});
-            // Parameter param = new Parameter[1];
-            Parameter param = TcapFactory.createParameter();
-            param.setTag(0x10);
-            param.setTagClass(0x00);
-            param.setParameters(params);
-            param.setPrimitive(false);
-            ((Invoke) componentsToSend[0]).setParameter(param);
-            tcbm.setComponent(componentsToSend);
-        //}
-         */
-
+    	// define MAP messages (MAP.asn file)
+    	this.mapComponent = new Component();
+    	Invoke invoke = new Invoke();
+    	
+    	Operation op = new Operation();
+    	OperationLocalvalue opLocal = new OperationLocalvalue();
+    	opLocal.setValue(46L);
+    	op.selectLocalValue(opLocal);
+    	invoke.setOpCode(op);
+    	
+    	InvokeIdType invType = new InvokeIdType(); 
+    	invType.setValue(1);
+    	invoke.setInvokeID(invType);
+    	
+    	InvokeParameter invokeParameter = new InvokeParameter();
+    	invoke.setInvokeparameter(invokeParameter);
+    	Mo_forwardSM_Arg moforwardSM_Arg = new Mo_forwardSM_Arg();
+    	Sm_RP_DA sm_rp_da = new Sm_RP_DA();
+    	ServiceCentreAddress serviceCentreAddress = new ServiceCentreAddress();
+    	serviceCentreAddress.setValue(new byte[]{(byte)0x91,(byte)0x33,(byte)0x66,(byte)0x60,(byte)0x05,(byte)0x67,(byte)0xf9});
+    	sm_rp_da.selectServiceCentreAddressDA(serviceCentreAddress);
+    	moforwardSM_Arg.setSm_RP_DA(sm_rp_da);
+    	
+    	Sm_RP_OA sm_rp_oa = new Sm_RP_OA();
+    	ISDN_AddressString isdn_AddressString = new ISDN_AddressString();
+    	isdn_AddressString.setValue(new byte[]{(byte)0x91,(byte)0x33,(byte)0x66,(byte)0x31,(byte)0x70,(byte)0x71,(byte)0xf3});
+    	sm_rp_oa.selectMsisdn(isdn_AddressString);
+    	moforwardSM_Arg.setSm_RP_OA(sm_rp_oa);
+    	
+    	Sm_RP_UI sm_rp_ui = new Sm_RP_UI();
+    	sm_rp_ui.setValue(new byte[]{(byte)0x11,(byte)0x08,(byte)0x0b,(byte)0x91,(byte)0x33,(byte)0x66,(byte)0x60,(byte)0x05,(byte)0x67,(byte)0xf7,(byte)0x00,(byte)0x00,(byte)0xa9,(byte)0x06,(byte)0xf3,(byte)0xf9,(byte)0x7c,(byte)0x3e,(byte)0x9f,(byte)0x03});
+    	moforwardSM_Arg.setSm_RP_UI(sm_rp_ui);
+    	
+    	invokeParameter.setValue(moforwardSM_Arg);
+    	this.mapComponent.selectInvoke(invoke);
     }
 
     public Array encode() throws Exception 
     {
-       IEncoder<com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.TCMessage> encoder;
-       encoder = CoderFactory.getInstance().newEncoder("BER");
-       ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-       encoder.encode((com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.TCMessage) tcMessage, outputStream);
-       Array array =Array.fromHexString(getHexString(outputStream.toByteArray()));
-       return array;
+    	/* BUG en encodage avec BN : Remplacement par Mobicents
+        IEncoder<com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.DialoguePDU> encoderDialoguePDU = CoderFactory.getInstance().newEncoder("BER");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        encoderDialoguePDU.encode(this.dialoguePdu, outputStream);
+        
+        byte[] bytesPDU = outputStream.toByteArray();
+        String strPDU = getHexString(bytesPDU);
+        Array arrayDPDU = new DefaultArray(bytesPDU);
+		SupArray sup = new SupArray();
+		Array arraySep = new DefaultArray(new byte[]{(byte) 0x6b,(byte) 0x1e,(byte) 0x28,(byte) 0x1c,(byte) 0x06,(byte) 0x07,(byte) 0x00,(byte) 0x11,(byte) 0x86,(byte) 0x05,(byte) 0x01,(byte) 0x01,(byte) 0x01,(byte) 0xa0,(byte) 0x11});
+		sup.addFirst(arraySep);
+		sup.addLast(arrayDPDU);
+		String str = getHexString(sup.getBytes());
+    	*/
+    	
+    	// Library mobicents
+        AsnOutputStream aos = new AsnOutputStream();
+		this.dp.encode(aos);
+		Array sup = new DefaultArray(aos.toByteArray());
+		
+        DialoguePortion dialogPortion = new DialoguePortion();
+        dialogPortion.setValue(sup.getBytes());
+    	//dp.setValue(new byte[]{(byte) 0x6b,(byte) 0x1e,(byte) 0x28,(byte) 0x1c,(byte) 0x06,(byte) 0x07,(byte) 0x00,(byte) 0x11,(byte) 0x86,(byte) 0x05,(byte) 0x01,(byte) 0x01,(byte) 0x01,(byte) 0xa0,(byte) 0x11,(byte) 0x60,(byte) 0x0f,(byte) 0x80, (byte) 0x02, (byte) 0x07, (byte) 0x80, (byte) 0xa1, (byte) 0x09, (byte) 0x06, (byte) 0x07, (byte) 0x04, (byte) 0x00, (byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x15, (byte) 0x02});
+        this.tcapBegin.setDialoguePortion(dialogPortion);
+    	
+    	IEncoder<com.devoteam.srit.xmlloader.sigtran.ap.generated.map.Component> encoderMAP = CoderFactory.getInstance().newEncoder("BER");
+    	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        encoderMAP.encode(this.mapComponent, outputStream);
+        byte[] bytesMAP = outputStream.toByteArray();
+        String strMAP = getHexString(bytesMAP);
+        this.comps.get(0).setValue(bytesMAP);
+    	//this.comps.get(0).setValue(new byte[]{(byte) 0xa1,(byte) 0x30,(byte) 0x02,(byte) 0x01,(byte) 0x01,(byte) 0x02,(byte) 0x01,(byte) 0x2e,(byte) 0x30,(byte) 0x28, (byte) 0x84,(byte) 0x07,(byte) 0x91,(byte) 0x33,(byte) 0x66,(byte) 0x60,(byte) 0x05,(byte) 0x67,(byte) 0xf9,(byte) 0x82, (byte) 0x07,(byte) 0x91,(byte) 0x33,(byte) 0x66,(byte) 0x31,(byte) 0x70,(byte) 0x71,(byte) 0xf3,(byte) 0x04,(byte) 0x14,(byte) 0x11,(byte) 0x08,(byte) 0x0b,(byte) 0x91,(byte) 0x33,(byte) 0x66,(byte) 0x60,(byte) 0x05,(byte) 0x67,(byte) 0xf7,(byte) 0x00,(byte) 0x00,(byte) 0xa9,(byte) 0x06,(byte) 0xf3,(byte) 0xf9,(byte) 0x7c,(byte) 0x3e,(byte) 0x9f,(byte) 0x03});
+    	
+    	IEncoder<com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.TCMessage> encoderTCMessages = CoderFactory.getInstance().newEncoder("BER");
+		outputStream = new ByteArrayOutputStream();
+		encoderTCMessages.encode(this.tcMessage, outputStream);
+		Array array =Array.fromHexString(getHexString(outputStream.toByteArray()));
+		return array;
     } 
 
     public void decode(Array array) throws Exception
