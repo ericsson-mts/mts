35f0ba2093fd387488af1c7ec08b9a7d47925205
fabien.henry@ericsson.com <fabien.henry@ericsson.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2014-08-26 12:15:46 +0000

    New version

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/SetValueToAsn1.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/SetValueToAsn1.java
@@ -38,6 +38,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bn.types.ObjectIdentifier;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -47,10 +48,10 @@ import org.dom4j.io.SAXReader;
  *
  * @author fhenry
  */
-public class Asn1ToXml 
+public class SetValueToAsn1 
 {
 
-    public Asn1ToXml() 
+    public SetValueToAsn1() 
     {
     }
 
@@ -69,15 +70,13 @@ public class Asn1ToXml
         return document;
     }
 
-    public String toXML(Object objClass, int indent)  
+    public void setValue(Object objClass)  
     {
-    	indent = indent + 2;
-    	String ret = "";
 		try 
 		{
 			if (objClass ==  null)
 	    	{
-	    		return ret;
+	    		return;
 	    	}
 
 			String strClass = objClass.getClass().toString();
@@ -87,233 +86,78 @@ public class Asn1ToXml
 	    		strClass = strClass.substring(pos + 1);
 	    	}
 	    	
-			ret += "\n";
-			ret += indent(indent);
-	    	ret += "<";
-	    	ret += strClass;
-	    	ret +=">";
-	    	
 	        // parsing object methods 
-	    	Method[] methods = objClass.getClass().getDeclaredMethods();
+	    	//Method[] methods = objClass.getClass().getDeclaredMethods();
+	    	Field[] fields = objClass.getClass().getDeclaredFields();
 	    	//for (int i= methods.length - 1; i >=0; i--)
 	    	boolean simple = true;
-	    	for (int i= 0; i < methods.length; i++)
+	    	for (int i= 0; i < fields.length; i++)
 	    	{
-    			String name = methods[i].getName();
-    			if (name.startsWith("get") && !"getPreparedData".equals(name))
+	    		Field f = fields[i];
+	    		 f.setAccessible(true);
+    			if (true)
     			{
-    				Object subObject = methods[i].invoke(objClass);
-    				if (subObject == null) 
-					{
-    					continue;
-					}
-    				Class subClass = subObject.getClass();
-    				if (subClass != null && subClass.getCanonicalName().equals("java.lang.Boolean"))
+    				String name = f.getType().getCanonicalName();
+    				if (name != null && name.equals("org.bn.coders.IASN1PreparedElementData") )
     				{
-    					ret += "<Boolean>"; 
-    					ret +=subObject.toString();
-    					ret += "</Boolean>";
-    				} 
-    				else if (subClass != null && subClass.getCanonicalName().equals("java.lang.Long"))
-    				{
-    					ret += "<Long>";
-    					ret +=subObject.toString();
-    					ret += "</Long>";
+    					// nothing to do
     				}
-    				else if (subClass != null && subClass.getCanonicalName().equals("java.lang.Integer"))
+    				else if (name != null && name.equals("java.lang.Boolean"))
     				{
-    					ret += "<Integer>";
-    					ret +=subObject.toString();
-    					ret += "</Integer>";
+    					f.set(objClass, Boolean.valueOf("true").booleanValue());
     				} 
-    				else if (subClass != null && subClass.getCanonicalName().equals("java.lang.String"))
+    				else if (name != null && name.equals("java.lang.Long"))
     				{
-    					ret += "<String>";
-    					ret +=subObject.toString();
-    					ret += "</String>";
+    					f.set(objClass, Long.parseLong("11111111111111"));
     				}
-    				else if (subClass != null && subClass.getCanonicalName().equals("byte[]"))
+    				else if (name != null && name.equals("java.lang.Integer"))
     				{
-    					byte[] bytes = (byte[]) subObject;
-    					ret += "<Bytes>";
-    					ret += Utils.toHexaString(bytes, "");
-    					ret += "</Bytes>";
+    					f.set(objClass, Integer.parseInt("7777"));
+    				} 
+    				else if (name != null && name.equals("java.lang.String"))
+    				{
+    					f.set(objClass, "0.1.2.3.4.5.6.7.8.9");
+    				}
+    				else if (name != null && name.equals("byte[]"))
+    				{
+    					byte[] bytes = new byte[]{0,1,2,3,4,5,6,7,8,9};
+    					f.set(objClass, bytes);
+    				}
+    				else if (name != null && name.equals("java.util.Collection"))
+    				{
+    					ArrayList list = new ArrayList();
+    					f.set(objClass, list);
+    				}
+    				else if (name != null && name.equals("org.bn.types.ObjectIdentifier"))
+    				{
+    					ObjectIdentifier objId = new ObjectIdentifier();
+    					objId.setValue("0.1.2.3.4.5.6.7.8.9");
+    					f.set(objClass, objId);
     				}
     				else
     				{
-    					ret += toXML(subObject, indent);
-    					simple = false;
+    					try
+    					{
+    						Class thisClass = Class.forName(name);
+    						// get an instance
+            		        Object iClass = thisClass.newInstance();
+        					setValue(iClass);
+        					f.set(objClass, iClass);
+    					}
+    					catch (Exception e)
+    					{
+    						// nothing to do
+    						break;
+    					}
        				}
     			}
 			}
-	    	
-			if (!simple)
-			{
-		    	ret += "\n";
-		    	ret += indent(indent);
-			}
-	    	ret += "</";
-	    	ret += strClass;
-	    	ret += ">";
-	    	
 		} 
 		catch (Exception e) 
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-
-    	return ret;
-    }
-    
-    /**
-     * generates a string of nb*"    " (four spaces nb times), used for intentation in printAvp
-     */
-    private static String indent(int nb)
-    {
-        String str = "";
-        for (int i = 0; i < nb; i++)
-        {
-            str += " ";
-        }
-        return str;
     }
 
-    public void initObject(Object objClass, Element root, String ClasseName) throws InvocationTargetException, ClassNotFoundException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InstantiationException 
-    {
-        // parsing XML
-        List<Element> children = root.elements();
-        for (Element element : children) 
-        {
-            Class thisClass = objClass.getClass();
-            Field field = this.findField(objClass, element);
-            initField(objClass, element, field, ClasseName);
-        }
-    }
-
-    public Object instanceClass(String Classe, String ClasseName) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
-        if (!Classe.contains(ClasseName)) 
-        {
-            ClasseName = ClasseName + Classe;
-        }
-        else 
-        {
-            ClasseName = Classe;
-        }
-        Class thisClass = Class.forName(ClasseName);
-        // get an instance
-        Object iClass = thisClass.newInstance();
-        return iClass;
-    }
-
-    public Field findField(Object objClass, Element element) 
-    {
-        for (Field field : objClass.getClass().getDeclaredFields()) 
-        {
-            if (element.getName().equals("instance")) 
-            {
-                return field;
-            }
-            if (field.getName().equals(element.getName())) {
-                return field;
-            }
-        }
-        return null;
-    }
-
-    public Object parseField(Element element, String type, String className) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException, InstantiationException, InvocationTargetException, NoSuchMethodException 
-    {
-        if (type.equals("java.lang.Boolean")||type.equals("boolean"))  
-        {
-            return Boolean.valueOf(element.getTextTrim()).booleanValue();
-        }
-        else if (type.equals("java.lang.String")||type.equals("String")) 
-        {
-            return element.getTextTrim();
-        }
-        else if (type.equals("java.lang.Integer")||type.equals("int")) 
-        {
-            return Integer.parseInt(element.getTextTrim());
-        }
-        else if (type.equals("java.lang.Float")||type.equals("float"))  
-        {
-            return Float.parseFloat(element.getTextTrim());
-        }
-        else if (type.equals("java.lang.Short")||type.equals("short"))  
-        {
-            return Short.parseShort(element.getTextTrim());
-        }
-        else if (type.equals("java.lang.Long")||type.equals("long"))  
-        {
-            return Long.parseLong(element.getTextTrim());
-        }
-        else if (type.equals("java.lang.Byte")||type.equals("byte"))  
-        {
-            return Byte.parseByte(element.getTextTrim());
-        }
-        else if (type.equals("byte[]")) 
-        {
-            return new DefaultArray(Utils.parseBinaryString("h" + element.getTextTrim())).getBytes();
-        }
-        else 
-        {
-            String classNameCurrent = type.substring(type.lastIndexOf(".") + 1);
-            if ((type.contains(className)) && (!(type.equals(className + classNameCurrent)))) 
-            {
-                // static class : h225.h323_className$staticClass
-                type = type.substring(0, type.lastIndexOf(".")) + "$" + type.substring(type.lastIndexOf(".") + 1);
-            }
-            if (!type.contains(className)) 
-            {
-                className = "";
-            }
-            Object obj = Class.forName(type).newInstance();
-            Object objComplexClass = this.instanceClass(obj.getClass().getName(), className);
-            initObject(objComplexClass, element, className);
-            return objComplexClass;
-        }
-    }
-
-    public void initField(Object objClass, Element element, Field field, String ClasseName) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException, InstantiationException, InvocationTargetException, NoSuchMethodException 
-    {
-        // si le champ est privé, pour y accéder
-        field.setAccessible(true);
-        // pour ne pas traiter les static
-        if (field.toGenericString().contains("static")) 
-        {
-            return;
-        }
-        if (field.getType().getCanonicalName().contains("Collection")) 
-        {
-            // type DANS la collection
-
-            // Récupérer le type des élements de la collection
-            Type[] elementParamTypeTab = ((ParameterizedType) field.getGenericType()).getActualTypeArguments();
-
-            // Exception si la collection n'a pas un seul argument
-            if (elementParamTypeTab.length != 1) 
-            {
-                throw new RuntimeException("Message d'erreur");
-            }
-
-            Class collectionElementType = (Class) elementParamTypeTab[0];
-
-            // creer la collection
-            ArrayList<Object> listInstance = new ArrayList<Object>();
-
-            // parcourir les enfants <instance> de element
-            List<Element> children = element.elements("instance");
-            for (Element elementInstance : children) 
-            {
-                // pour chaque <instance>
-                listInstance.add(parseField(elementInstance, collectionElementType.getCanonicalName(), ClasseName));
-            }
-            // set la collection dans le field
-            field.set(objClass, listInstance);
-        }
-        else 
-        {
-            field.set(objClass, parseField(element, field.getType().getCanonicalName(), ClasseName));
-        }
-    }
 }
