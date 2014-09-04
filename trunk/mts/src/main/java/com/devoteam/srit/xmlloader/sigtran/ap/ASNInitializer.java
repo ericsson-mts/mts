b029345a2028e44f60590b4f3aee3ab0c2c30bdb
fabien.henry@ericsson.com <fabien.henry@ericsson.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2014-09-04 09:44:04 +0000

    Nouvelle version

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/ASNInitializer.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/ASNInitializer.java
@@ -74,128 +74,142 @@ public class ASNInitializer
         return document;
     }
 
-    public void setValue(Object objClass)  
+    public void setValue(Object objClass) throws Exception  
     {
-		try 
-		{
 		if (objClass ==  null)
     	{
 	    		return;
 	    	}
 
-			String strClass = objClass.getClass().toString();
-	    	int pos = strClass.lastIndexOf('.');
-	    	if (pos >= 0)
+		String strClass = objClass.getClass().toString();
+    	int pos = strClass.lastIndexOf('.');
+    	if (pos >= 0)
+    	{
+    		strClass = strClass.substring(pos + 1);
+    	}
+    	
+        // parsing object object fields 
+    	Field[] fields = objClass.getClass().getDeclaredFields();
+    	for (int i= 0; i < fields.length; i++)
+    	{
+    		Field f = fields[i];
+    		f.setAccessible(true);
+    		//System.out.println(f);
+
+    		String typeField = f.getType().getCanonicalName();
+    		String typeName  = f.getName();
+        	if (typeField == null)
+        	{
+        		// nothing to do
+        	}
+        	else if (typeField != null && typeField.equals("org.bn.coders.IASN1PreparedElementData") )
 	    	{
-	    		strClass = strClass.substring(pos + 1);
+	    		// nothing to do
 	    	}
-	    	
-	        // parsing object object fields 
-	    	Field[] fields = objClass.getClass().getDeclaredFields();
-	    	for (int i= 0; i < fields.length; i++)
+        	else if (typeName != null && typeName.equals("privateExtensionList") )
 	    	{
-	    		Field f = fields[i];
-	    		f.setAccessible(true);
-	    		//System.out.println(f);
-
-				String typeField = f.getType().getCanonicalName();
-				if (typeField != null && typeField.equals("org.bn.coders.IASN1PreparedElementData") )
+        		// nothing to do
+        		// because error when decoding this field
+	    	}
+        	/*
+        	else if (typeName != null && typeName.equals("extId") )
+	    	{
+        		// nothing to do
+        		// because error when decoding this field
+	    	}
+        	else if (typeName != null && typeName.equals("extType") )
+	    	{
+        		// nothing to do
+        		// because error when decoding this field
+	    	}
+	    	*/	    	
+        	else if (typeField != null && typeField.equals("java.util.Collection"))
+			{
+				ParameterizedType genType = (ParameterizedType) f.getGenericType();
+				Type[] typeActualTypeArg = genType.getActualTypeArguments();
+				LinkedList list = new LinkedList();
+				if (typeActualTypeArg.length > 0)
 				{
-					continue;
-					// nothing to do
-				}
-				else if (typeField != null && typeField.equals("byte[]"))
-				{
-					byte[] bytes = new byte[]{0,1,0,1,0,1,0,1};
-					//byte[] bytes = new byte[]{1};
-					f.set(objClass, bytes);
-					continue;
-				}
-				else if (typeField != null && (typeField.equals("java.lang.Boolean") || typeField.equals("boolean")))
-				{
-					f.set(objClass, Boolean.valueOf("true").booleanValue());
-					continue;
-				} 
-				else if (typeField != null && (typeField.equals("java.lang.Long") || typeField.equals("long")))
-				{
-					f.set(objClass, Long.parseLong("11111111111111"));
-					continue;
-				}
-				else if (typeField != null && (typeField.equals("java.lang.Integer") || typeField.equals("int")))
-				{
-					f.set(objClass, Integer.parseInt("11"));
-					continue;
-				} 
-				else if (typeField != null && typeField.equals("java.lang.String"))
-				{
-					f.set(objClass, "0.1.2.3.4.5.6.7.8.9");
-					continue;
-				}
-				else if (typeField != null && typeField.equals("java.util.Collection"))
-				{
-					ParameterizedType genType = (ParameterizedType) f.getGenericType();
-					Type[] typeActualTypeArg = genType.getActualTypeArguments();
-					LinkedList list = new LinkedList();
-					if (typeActualTypeArg.length > 0)
-					{
-						String tabClassName = ((Class) typeActualTypeArg[0]).getCanonicalName();
-						for (int j = 0; j <= 1; j++)
-						{		
-    						Class tabClass = Class.forName(tabClassName);
-
-							Object tabObject = null;
-							Constructor constr = tabClass.getConstructor();
-							if (constr !=null)
-							{
-								constr.setAccessible(true);
-								tabObject = constr.newInstance();
-							}
-							else
-							{
-								tabObject = tabClass.newInstance();
-							}
-            		        setValue(tabObject);
-            		        list.add(tabObject);
-						}
-						f.set(objClass, list);
+					for (int j = 0; j <= 2; j++)
+					{		
+						Class tabClass = (Class) typeActualTypeArg[0];
+						Object tabObject = getSubObject(objClass, tabClass);	
+			    		if (tabObject != null)
+			    		{
+			    			list.add(tabObject);
+			    		}
 					}
-					continue;
-				}
-				else if (typeField != null && typeField.equals("org.bn.types.ObjectIdentifier"))
-				{
-					ObjectIdentifier objId = new ObjectIdentifier();
-					objId.setValue("0.1.2.3.4.5.6.7.8.9");
-					f.set(objClass, objId);
-					continue;
-				}
-				else if (typeField != null && typeField.endsWith("EnumType"))
-				{
-					Class[] classes = objClass.getClass().getClasses();
-					Object[] objects = null;
-					if (classes.length >= 1)
-					{
-						objects = classes[0].getEnumConstants();
-						f.set(objClass, objects[0]);
-					}
-					continue;
-				}
-				else
-				{
-					Class subClass = f.getType();
-					Constructor constr = subClass.getConstructor();
-					constr.setAccessible(true);
-					Object subObj = constr.newInstance();
-					setValue(subObj);
-					f.set(objClass, subObj);
-					continue;
+					f.set(objClass, list);
 				}
 			}
-		} 
-		catch (Exception e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+			else
+			{
+	    		Object subObject = getSubObject(objClass, f.getType());
+	    		if (subObject != null)
+	    		{
+	    			f.set(objClass, subObject);
+	    		}
+			}				
+		}
     }
 
+    
+    
+    
+    private Object getSubObject(Object obj, Class subClass) throws Exception
+    {
+    	String type = subClass.getCanonicalName();
+    	if (type.equals("org.bn.coders.IASN1PreparedElementData") )
+    	{
+    		return null;
+    		// nothing to do
+    	}
+    	else if (type.equals("byte[]"))
+    	{
+    		byte[] bytes = new byte[]{4,8,0,1,2,3,4,5,6,7};
+    		//byte[] bytes = new byte[]{1};
+    		return bytes;
+    	}
+    	else if (type.equals("java.lang.Boolean") || type.equals("boolean"))
+    	{
+			return Boolean.valueOf("true").booleanValue();
+		} 
+		else if (type.equals("java.lang.Long") || type.equals("long"))
+		{
+			return Long.parseLong("11111111111111");
+		}
+		else if (type.equals("java.lang.Integer") || type.equals("int"))
+		{
+			return Integer.parseInt("11");
+		} 
+		else if (type.equals("java.lang.String"))
+		{
+			return "0.1.2.3.4.5.6.7.8.9";
+		}
+		else if (type.equals("org.bn.types.ObjectIdentifier"))
+		{
+			ObjectIdentifier objId = new ObjectIdentifier();
+			objId.setValue("0.1.2.3.4.5.6.7.8.9");
+			return  objId;
+		}
+		else if (type.endsWith("EnumType"))
+		{
+			Class[] classes = obj.getClass().getClasses();
+			Object[] objects = null;
+			if (classes.length >= 1)
+			{
+				objects = classes[0].getEnumConstants();
+			}
+			return objects[0];
+		}
+		else
+		{
+			Constructor constr = subClass.getConstructor();
+			constr.setAccessible(true);
+			Object subObj = constr.newInstance();
+			setValue(subObj);
+			return subObj;
+		}
+    }
+    
 }
