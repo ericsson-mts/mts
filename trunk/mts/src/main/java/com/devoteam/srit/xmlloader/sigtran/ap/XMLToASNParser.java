b8bc80cfba3de19693552d82d9db06c7013edd7c
fabien.henry@ericsson.com <fabien.henry@ericsson.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2014-09-16 08:19:04 +0000

    Premiere version d'encapsulation (DialogueOC => ExternalPDU) pb 4B a la place de 6B pour le tag de DialoguePortion

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/XMLToASNParser.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/sigtran/ap/XMLToASNParser.java
@@ -26,9 +26,13 @@ package com.devoteam.srit.xmlloader.sigtran.ap;
 import com.devoteam.srit.xmlloader.core.log.GlobalLogger;
 import com.devoteam.srit.xmlloader.core.log.TextEvent;
 import com.devoteam.srit.xmlloader.core.utils.Utils;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.DialogueOC;
+import com.devoteam.srit.xmlloader.sigtran.ap.generated.tcap.DialoguePortion;
 
+import gp.utils.arrays.Array;
 import gp.utils.arrays.DefaultArray;
 
+import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
@@ -36,6 +40,8 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bn.CoderFactory;
+import org.bn.IEncoder;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -79,7 +85,7 @@ public class XMLToASNParser
         return document;
     }
 
-    public void initObject(Object objClass, Element root, String ClasseName) throws InvocationTargetException, ClassNotFoundException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InstantiationException 
+    public void initObject(Object objClass, Element root, String ClasseName) throws Exception 
     {
         // parsing XML
         List<Element> children = root.elements();
@@ -139,7 +145,7 @@ public class XMLToASNParser
         return null;
     }
 
-    public Object parseField(Element element, String type, Object object, String className) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException, InstantiationException, InvocationTargetException, NoSuchMethodException 
+    public Object parseField(Element element, String type, Object object, String className) throws Exception 
     {
         if (type.equals("java.lang.Boolean")||type.equals("boolean"))  
         {
@@ -193,6 +199,25 @@ public class XMLToASNParser
 			}
             return null;
         }
+        else if (type.contains("DialogueOC"))
+        {
+            Object obj = Class.forName(type).newInstance();
+
+            Object objEmbeded = Class.forName(className + "ExternalPDU").newInstance();
+            Object objEmbbededClass = this.instanceClass(objEmbeded.getClass().getName(), className);
+            initObject(objEmbbededClass, (Element) element.elements().get(0), className);
+        	
+            // encode ASN1 object into binary
+        	IEncoder<Object> encoderEmbedded = CoderFactory.getInstance().newEncoder("BER");
+        	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        	encoderEmbedded.encode(objEmbbededClass, outputStream);
+            byte[] bytesEmbedded = outputStream.toByteArray();
+            Array arraybytesEmbedded = new DefaultArray(bytesEmbedded);
+            
+            ((DialogueOC) obj).setValue(bytesEmbedded);
+            
+            return obj;
+        }
         else 
         {
             String classNameCurrent = type.substring(type.lastIndexOf(".") + 1);
@@ -201,18 +226,19 @@ public class XMLToASNParser
                 // static class : h225.h323_className$staticClass
                 type = type.substring(0, type.lastIndexOf(".")) + "$" + type.substring(type.lastIndexOf(".") + 1);
             }
+         
             if (!type.contains(className)) 
             {
                 className = "";
             }
             Object obj = Class.forName(type).newInstance();
-            Object objComplexClass = this.instanceClass(obj.getClass().getName(), className);
-            initObject(objComplexClass, element, className);
-            return objComplexClass;
+            //Object objComplexClass = this.instanceClass(obj.getClass().getName(), className);
+            initObject(obj, element, className);
+            return obj;
         }
     }
 
-    public void initField(Object objClass, Element element, Field field, String ClasseName) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException, InstantiationException, InvocationTargetException, NoSuchMethodException 
+    public void initField(Object objClass, Element element, Field field, String className) throws Exception 
     {
         // si le champ est privé, pour y accéder
         field.setAccessible(true);
@@ -223,7 +249,24 @@ public class XMLToASNParser
         {
             return;
         }
-        if (field.getType().getCanonicalName().contains("Collection")) 
+        /*
+        else if (field.getType().getCanonicalName().contains("DialoguePortion"))
+        {
+            Object obj = Class.forName(className + "ExternalPDU").newInstance();
+            Object objEmbbededClass = this.instanceClass(obj.getClass().getName(), className);
+            initObject(objEmbbededClass, (Element) element.elements().get(0), className);
+        	
+            // encode ASN1 object into binary
+        	IEncoder<Object> encoderEmbedded = CoderFactory.getInstance().newEncoder("BER");
+        	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        	encoderEmbedded.encode(objEmbbededClass, outputStream);
+            byte[] bytesEmbedded = outputStream.toByteArray();
+            Array arraybytesEmbedded = new DefaultArray(bytesEmbedded);
+            
+            ((DialoguePortion) objClass).setValue(bytesEmbedded);
+        }
+        */
+        else if (field.getType().getCanonicalName().contains("Collection")) 
         {
             // type DANS la collection
 
@@ -246,7 +289,7 @@ public class XMLToASNParser
             for (Element elementInstance : children) 
             {
                 // pour chaque <instance>
-                listInstance.add(parseField(elementInstance, collectionElementType.getCanonicalName(), objClass, ClasseName));
+                listInstance.add(parseField(elementInstance, collectionElementType.getCanonicalName(), objClass, className));
             }
             /*
             List<Element> children1 = element.elements("value");
@@ -262,7 +305,7 @@ public class XMLToASNParser
         }
         else 
         {
-            field.set(objClass, parseField(element, field.getType().getCanonicalName(), objClass, ClasseName));
+            field.set(objClass, parseField(element, field.getType().getCanonicalName(), objClass, className));
         }
     }
 }
