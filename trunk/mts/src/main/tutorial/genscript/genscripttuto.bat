04cfca50bbdc68801b45788ebf7e460201755143
fabien.henry@ericsson.com <fabien.henry@ericsson.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2015-08-10 08:32:49 +0000

    Ajout OPTIONS

--- trunk/mts/src/main/tutorial/genscript/genscripttuto.bat
+++ trunk/mts/src/main/tutorial/genscript/genscripttuto.bat
@@ -76,7 +76,8 @@ echo ------------------------------
 
 cd ..\bin
 
+set OPTIONS=-level:WARN -stor:file -gen:false -show:false
 set ARGS0="../tutorial/genscript/out/testout.xml"
-call startCmd.bat %ARGS0%
+call startCmd.bat %ARGS0% -seq %OPTIONS%
 
 pause
