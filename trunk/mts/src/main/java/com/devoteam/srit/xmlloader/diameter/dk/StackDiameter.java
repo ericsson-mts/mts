811256437cd82333756f993e07919b558adaf9d6
fabien.henry@ericsson.com <fabien.henry@ericsson.com@32ca6872-2e73-404e-9817-36692b89cbe1>
2015-08-10 16:31:33 +0000

    Déplacer ChannelDiameter dans package dk

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/diameter/dk/StackDiameter.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/diameter/dk/StackDiameter.java
@@ -23,10 +23,6 @@
 
 package com.devoteam.srit.xmlloader.diameter.dk;
 
-import org.dom4j.Element;
-
-import com.devoteam.srit.xmlloader.core.Runner;
-import com.devoteam.srit.xmlloader.core.protocol.Channel;
 import com.devoteam.srit.xmlloader.diameter.StackDiamCommon;
 
 /**
@@ -42,11 +38,4 @@ public class StackDiameter extends StackDiamCommon
         super();
     }
             
-    /** Creates a channel specific to each Stack */
-    @Override
-    public Channel parseChannelFromXml(Element root, Runner runner, String protocol) throws Exception
-    {
-        return null;
-    }
-
 }
