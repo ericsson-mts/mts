206bd9e9791361f7382ea632942897c4d29cfa4e
efabhen <efabhen@32ca6872-2e73-404e-9817-36692b89cbe1>
2015-12-28 15:27:39 +0000

    Corriger bug genscript avec EAP-Payload

--- mts/src/main/tutorial/genscript/genscripttest.bat
+++ mts/src/main/tutorial/genscript/genscripttest.bat
@@ -4,6 +4,17 @@ rmdir out /s/q
 
 cd ..\..\bin
 
+set ARGS0="DIAMETER:164.48.219.44:50708" 
+set ARGS1="../tutorial/genscript/trace_DIAM_swm_ok.pcap"
+set ARGS2="../tutorial/genscript/out/testout.xml"
+call genscript.bat %ARGS0% %ARGS1% %ARGS2%
+
+set ARGS0="DIAMETER:164.48.219.44:13868" 
+set ARGS1="../tutorial/genscript/trace_DIAM_swm_ok.pcap"
+set ARGS2="../tutorial/genscript/out/testout.xml"
+call genscript.bat %ARGS0% %ARGS1% %ARGS2%
+
+
 echo --------------------------------------------
 echo -- TEST SIP SUR UDP AVEC PORT PAR DEFAULT --
 echo --------------------------------------------
@@ -160,6 +171,16 @@ set ARGS1="../tutorial/genscript/trace_DIAM_Cisco_CSG.pcap"
 set ARGS2="../tutorial/genscript/out/testout.xml"
 rem call genscript.bat %ARGS0% %ARGS1% %ARGS2%
 
+
+set ARGS0="DIAMETER:164.48.219.44:50708" 
+set ARGS1="../tutorial/genscript/trace_DIAM_swm_ok.pcap"
+set ARGS2="../tutorial/genscript/out/testout.xml"
+call genscript.bat %ARGS0% %ARGS1% %ARGS2%
+
+set ARGS0="DIAMETER:164.48.219.44:13868" 
+set ARGS1="../tutorial/genscript/trace_DIAM_swm_ok.pcap"
+set ARGS2="../tutorial/genscript/out/testout.xml"
+call genscript.bat %ARGS0% %ARGS1% %ARGS2%
 
 echo ------------------------------
 echo -- TEST DES SCRIPTS GENERES --
