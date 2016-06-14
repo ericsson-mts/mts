d8cfd68e70ea18b4342dfafd07a39a4ec1fda81b
efabhen <efabhen@32ca6872-2e73-404e-9817-36692b89cbe1>
2016-06-14 16:02:52 +0000

    Implémenter décodage

--- trunk/mts/src/main/java/com/devoteam/srit/xmlloader/core/coding/binary/coap/ElementCOAPOption.java
+++ trunk/mts/src/main/java/com/devoteam/srit/xmlloader/core/coding/binary/coap/ElementCOAPOption.java
@@ -61,11 +61,32 @@ public class ElementCOAPOption extends ElementAbstract
 			ElementCOAPMessage messageCOAP = (ElementCOAPMessage) this.parentElement;
 			currentTag = messageCOAP.getCurrentTag();
 		}
-		
+
 		Array deltaLengthArray = array.subArray(0, 1);
 	    int deltaLength = new Integer08Array(deltaLengthArray).getValue();
+	    int delta = deltaLength / 16; 
 	    
-	    this.tag = currentTag + (deltaLength / 16);
+    	if (delta <= 12)
+    	{
+    	    this.tag = currentTag + (deltaLength / 16);
+    	}
+    	else if (delta == 13)
+    	{
+    		Array delta1LengthArray = array.subArray(1, 1);
+    	    int delta1 = new Integer08Array(delta1LengthArray).getValue();
+    	    this.tag = currentTag + delta1 + 13;
+    	}
+    	else if (delta == 14)
+    	{
+    		Array delta2LengthArray = array.subArray(1, 2);
+    	    int delta2 = new Integer16Array(delta2LengthArray).getValue();
+    	    this.tag = currentTag + delta2 + 14 + 255;
+    	}
+    	else
+    	{
+    		this.tag = 1000000;
+    	}    	    	
+
 	    return this.tag;		
 	}
     
@@ -87,12 +108,39 @@ public class ElementCOAPOption extends ElementAbstract
 
 		Array deltaLengthArray = array.subArray(0, 1);
 	    int deltaLength = new Integer08Array(deltaLengthArray).getValue();
+	    int delta = deltaLength / 16; 
 	    
-	    this.tag = currentTag + (deltaLength / 16); 
-	    int length = deltaLength % 16;
-	    
-	    Array dataArray = array.subArray(1, length);
-	    	
+	    int length = 0;
+	    length = deltaLength % 16;
+	    Array dataArray;
+    	if (delta <= 12)
+    	{
+    	    this.tag = currentTag + (deltaLength / 16);
+    	    dataArray = array.subArray(1, length);
+    	    length = length + 1;
+    	}
+    	else if (delta == 13)
+    	{
+    		Array delta1LengthArray = array.subArray(1, 1);
+    	    int delta1 = new Integer08Array(delta1LengthArray).getValue();
+    	    this.tag = currentTag + delta1 + 13;
+    	    dataArray = array.subArray(2, length);
+    	    length = length + 2;
+    	}
+    	else if (delta == 14)
+    	{
+    		Array delta2LengthArray = array.subArray(1, 2);
+    	    int delta2 = new Integer16Array(delta2LengthArray).getValue();
+    	    this.tag = currentTag + delta2 + 14 + 255;
+    	    dataArray = array.subArray(3, length);
+    	    length = length + 3;
+    	}
+    	else
+    	{
+    		dataArray = array.subArray(1);
+    		length = array.length;
+    	}    	    	
+	    	  		    
 	    decodeFieldsTagElementsFromArray(dataArray, dictionary);
 	    
 		// set the current because tag encoding is relative to the previous one
@@ -102,7 +150,7 @@ public class ElementCOAPOption extends ElementAbstract
 			messageCOAP.setCurrentTag(this.tag);
 		}
 	    
-	    return length + 1;
+	    return length;
     }
 
 	@Override    
@@ -140,16 +188,16 @@ public class ElementCOAPOption extends ElementAbstract
     	else if (deltaTag <= 65535)
     	{
 	    	int deltaLength = (14) * 16 + length;
-	    	Integer08Array delta1Array = new Integer08Array(deltaLength);
-	    	sup.addLast(delta1Array);
-	    	Integer16Array tag1Array = new Integer16Array(deltaTag - 14 -255);
-	    	sup.addLast(tag1Array);
+	    	Integer08Array delta2Array = new Integer08Array(deltaLength);
+	    	sup.addLast(delta2Array);
+	    	Integer16Array tag2Array = new Integer16Array(deltaTag - 14 -255);
+	    	sup.addLast(tag2Array);
     	}
     	else
     	{
 	    	int deltaLength = (15) * 16 + length;
-	    	Integer08Array delta1Array = new Integer08Array(deltaLength);
-	    	sup.addLast(delta1Array);
+	    	Integer08Array delta3Array = new Integer08Array(deltaLength);
+	    	sup.addLast(delta3Array);
     	}    	    	
 	    sup.addLast(this.fieldsArray);
 	    sup.addLast(this.subelementsArray);
