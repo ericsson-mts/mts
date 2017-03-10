1a4e34c95f776e8f900f11e589dc751c52e71b04
mpo22 <mpo22@32ca6872-2e73-404e-9817-36692b89cbe1>
2017-03-10 18:38:08 +0000

    #t8 sctp transport accessors documentation workflow refactoring

---
+++ branches/t8-sctp/mts/src/main/doc-tools/generate_xsd.bat
@@ -0,0 +1,2 @@
+..\..\..\3pp\apache-ant-1.7.0\apache-ant-1.7.0\bin\ant.bat clean,generate_xsd
+pause
--- branches/t8-sctp/mts/src/main/generate_xsd.bat
+++
@@ -1,2 +0,0 @@
-..\..\3pp\apache-ant-1.7.0\apache-ant-1.7.0\bin\ant.bat clean,generate_xsd
-pause
