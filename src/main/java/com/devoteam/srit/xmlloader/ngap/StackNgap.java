250b1c430451a1df448621f73b09cfb5cd1b6f8f
Gaetan Gogail <gaetan.gogail@ericsson.com>
2019-11-07 00:11:51 +0100

    externalisation de la conf NAS

--- src/main/java/com/devoteam/srit/xmlloader/ngap/StackNgap.java
+++ src/main/java/com/devoteam/srit/xmlloader/ngap/StackNgap.java
@@ -2,6 +2,7 @@ package com.devoteam.srit.xmlloader.ngap;
 
 import com.devoteam.srit.xmlloader.core.protocol.Msg;
 import com.devoteam.srit.xmlloader.core.protocol.Stack;
+import com.devoteam.srit.xmlloader.core.utils.Config;
 import com.devoteam.srit.xmlloader.core.utils.XMLElementAVPParser;
 import com.devoteam.srit.xmlloader.core.utils.XMLElementReplacer;
 import com.devoteam.srit.xmlloader.sctp.DataSctp;
@@ -14,7 +15,10 @@ import dk.i1.diameter.node.Node;
 import gp.utils.arrays.DefaultArray;
 
 //import java.rmi.registry.Registry;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Arrays;
 
 public class StackNgap extends Stack {
@@ -67,24 +71,21 @@ public class StackNgap extends Stack {
         return asn1Translator;
     }
 
-    public AbstractMessage createNASTranslator() throws IOException {
-//        commonRegistryNasTranslator.loadMessages(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS/dictionary5GSCommonMessages.yaml"));
-//        commonRegistryNasTranslator.loadInformationElements(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS/dictionary5GSCommonInformationElements.yaml"));
-//
-//        registry5GSNasTranslator.loadMessages(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS/dictionnary5GSMobilityManagementMessages.yaml"));
-//        registry5GSNasTranslator.loadInformationElements(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS/dictionnary5GSMobilityManagementInformationElements.yaml"));
-//
-//        registry5gsSessionManagementMessages.loadMessages(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS/dictionnary5GSSesionManagementMessages.yaml"));
-//        registry5gsSessionManagementMessages.loadInformationElements(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS/dictionnary5GSSesionManagementInformationElements.yaml"));
+    private AbstractMessage createNASTranslator() throws Exception {
 
-        commonRegistryNasTranslator.loadMessages(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS-152/dictionary5GSCommonMessages.yaml"));
-        commonRegistryNasTranslator.loadInformationElements(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS-152/dictionary5GSCommonInformationElements.yaml"));
+        Config config = Config.getConfigByName("ngap.properties");
+        String directory = "../conf/nas/grammar/5gs/NAS-5GS-"+config.getString("nas.VERSION","152");
 
-        registry5GSNasTranslator.loadMessages(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS-152/dictionnary5GSMobilityManagementMessages.yaml"));
-        registry5GSNasTranslator.loadInformationElements(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS-152/dictionnary5GSMobilityManagementInformationElements.yaml"));
 
-        registry5gsSessionManagementMessages.loadMessages(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS-152/dictionnary5GSSesionManagementMessages.yaml"));
-        registry5gsSessionManagementMessages.loadInformationElements(this.getClass().getResourceAsStream("/nas/grammar/5gs/NAS-5GS-152/dictionnary5GSSesionManagementInformationElements.yaml"));
+
+        commonRegistryNasTranslator.loadMessages(new FileInputStream(directory+"/dictionary5GSCommonMessages.yaml"));
+        commonRegistryNasTranslator.loadInformationElements(new FileInputStream(directory+"/dictionary5GSCommonInformationElements.yaml"));
+
+        registry5GSNasTranslator.loadMessages(new FileInputStream(directory+"/dictionnary5GSMobilityManagementMessages.yaml"));
+        registry5GSNasTranslator.loadInformationElements(new FileInputStream(directory+"/dictionnary5GSMobilityManagementInformationElements.yaml"));
+
+        registry5gsSessionManagementMessages.loadMessages(new FileInputStream(directory+"/dictionnary5GSSesionManagementMessages.yaml"));
+        registry5gsSessionManagementMessages.loadInformationElements(new FileInputStream(directory+"/dictionnary5GSSesionManagementInformationElements.yaml"));
 
 
         registry5GSNasTranslator.mergeRegistry(commonRegistryNasTranslator);
